93c30d87e0b44d851781d34049082234e9eb4011
Mauricio Banduk <mauricio.banduk@pier.digital>
2020-07-03 10:38:07 -0300

    Debug action

--- bin/publish_gem.sh
+++ bin/publish_gem.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 echo "Setting up gem credentials..."
-set +x
+# set +x
 mkdir -p ~/.gem
-
+echo '----------------------'
 cat << EOF > ~/.gem/credentials
 ---
 :github: Bearer ${GH_PERSONAL_ACCESS_TOKEN}
 :rubygems_api_key: ${RUBYGEMS_API_KEY}
 
 EOF
-
+echo '----------------------'
 chmod 0600 ~/.gem/credentials
-set -x
+# set -x
+
+echo '----------------------'
+cat ~/.gem/credentials
+echo '----------------------'
 
 echo "Installing dependencies..."
 bundle install > /dev/null 2>&1
