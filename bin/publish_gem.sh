95a44a895ba71f0411e7ef412669823583fabf89
Mauricio Banduk <mauricio.banduk@pier.digital>
2020-07-03 10:40:18 -0300

    Try to fix publish_gem script

--- bin/publish_gem.sh
+++ bin/publish_gem.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
 echo "Setting up gem credentials..."
-# set +x
+
+set +x
+
 mkdir -p ~/.gem
-echo '----------------------'
+
 cat << EOF > ~/.gem/credentials
 ---
 :github: Bearer ${GH_PERSONAL_ACCESS_TOKEN}
 :rubygems_api_key: ${RUBYGEMS_API_KEY}
-
 EOF
-echo '----------------------'
-chmod 0600 ~/.gem/credentials
-# set -x
 
-echo '----------------------'
-cat ~/.gem/credentials
-echo '----------------------'
+chmod 0600 ~/.gem/credentials
+
+set -x
 
 echo "Installing dependencies..."
 bundle install > /dev/null 2>&1
 
 echo "Running gem release task..."
-rake release
+exec rake release
