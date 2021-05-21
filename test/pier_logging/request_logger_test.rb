f9d4807af612260a65287c23222c65add03dd863
Leonardo Bighetti <leonardo.bighetti@gmail.com>
2021-05-21 17:10:57 -0300

    wip

--- test/pier_logging/request_logger_test.rb
+++ test/pier_logging/request_logger_test.rb
@@ -1,5 +1,20 @@
 require "test_helper"
 
 class PierLogging::RequestLoggerTest < Minitest::Test
-  subject { PierLogging::RequestLogger.new }
+
+
+  context "#log" do
+    setup do
+      PierLogging.request_logger_configuration.sensitive_keywords = [:blah]
+      PierLogging.request_logger_configuration.enabled = true
+      @logger = PierLogging::Logger.new(STDOUT)
+    end
+
+    subject { PierLogging::RequestLogger.new mock(), @logger }
+
+    should 'redact blah' do
+      args = [:test, 'foo', 'foo', {blah: 'foo'}, Time.now, Time.now]
+      subject.log args
+    end
+  end
 end
