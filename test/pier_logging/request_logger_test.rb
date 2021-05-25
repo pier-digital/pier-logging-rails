09cd3e1a1c9ad79a99d6f18bc4a6c68e8cf2d22d
Bruno Arakaki <brn.arakaki@gmail.com>
2021-05-25 19:03:48 -0300

    Fix tests

--- test/pier_logging/request_logger_test.rb
+++ test/pier_logging/request_logger_test.rb
@@ -1,20 +1,30 @@
 require "test_helper"
 
 class PierLogging::RequestLoggerTest < Minitest::Test
-
+  subject { PierLogging::RequestLogger.new(mock, @logger) }
 
   context "#log" do
     setup do
       PierLogging.request_logger_configuration.sensitive_keywords = [:blah]
       PierLogging.request_logger_configuration.enabled = true
-      @logger = PierLogging::Logger.new(STDOUT)
+      @logger = PierLogging::Logger.new($stdout)
+      env_mock = Rack::MockRequest.env_for
+      body = {blah: "foo", bluh: "plaft"}
+      @args = [env_mock, "status", {"Content-type": "12"}, body, Time.now, Time.now]
     end
 
-    subject { PierLogging::RequestLogger.new mock(), @logger }
+    teardown do
+      PierLogging.request_logger_configuration.enabled = false
+    end
 
-    should 'redact blah' do
-      args = [:test, 'foo', 'foo', {blah: 'foo', bluh: 'plaft'}, Time.now, Time.now]
-      subject.log args
+    should "redact sensitve keywords" do
+      @logger.expects(:info).with do |logged_args|
+        redacted_body = logged_args[:response][:body]
+        assert_equal "*", redacted_body[:blah]
+        assert_equal "plaft", redacted_body[:bluh]
+      end
+
+      subject.log(@args)
     end
   end
 end
