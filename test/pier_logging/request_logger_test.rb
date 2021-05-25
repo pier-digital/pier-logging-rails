2d41009da40aa42b4dca99c1c944b2a99c8ae813
Bruno Arakaki <brn.arakaki@gmail.com>
2021-05-25 19:38:59 -0300

    Testing nested keys

--- test/pier_logging/request_logger_test.rb
+++ test/pier_logging/request_logger_test.rb
@@ -8,23 +8,41 @@ class PierLogging::RequestLoggerTest < Minitest::Test
       PierLogging.request_logger_configuration.sensitive_keywords = [:blah]
       PierLogging.request_logger_configuration.enabled = true
       @logger = PierLogging::Logger.new($stdout)
-      env_mock = Rack::MockRequest.env_for
+      @env_mock = Rack::MockRequest.env_for
       body = {blah: "foo", bluh: "plaft"}
-      @args = [env_mock, "status", {"Content-type": "12"}, body, Time.now, Time.now]
+      @args = [@env_mock, "status", {"Content-type": "12"}, body, Time.now, Time.now]
     end
 
     teardown do
       PierLogging.request_logger_configuration.enabled = false
     end
 
-    should "redact sensitve keywords" do
-      @logger.expects(:info).with do |logged_args|
-        redacted_body = logged_args[:response][:body]
-        assert_equal "*", redacted_body[:blah]
-        assert_equal "plaft", redacted_body[:bluh]
-      end
+    context 'when sensitive keyword is at root' do
+      should "redact sensitive keywords" do
+        @logger.expects(:info).with do |logged_args|
+          redacted_body = logged_args[:response][:body]
+          assert_equal "*", redacted_body[:blah]
+          assert_equal "plaft", redacted_body[:bluh]
+        end
 
-      subject.log(@args)
+        subject.log(@args)
+      end
+    end
+
+    context 'when sensitive keyword is nested in array' do
+      setup do
+        body = {blirgh: [{blah: "foo"}, {bluh: "plaft"}]}
+        @args = [@env_mock, "status", {"Content-type": "12"}, body, Time.now, Time.now]
+      end
+      should "redact sensitve keywords" do
+        @logger.expects(:info).with do |logged_args|
+          redacted_body = logged_args[:response][:body]
+          assert_equal "*", redacted_body[:blirgh][0][:blah]
+          assert_equal "plaft", redacted_body[:blirgh][1][:bluh]
+        end
+
+        subject.log(@args)
+      end
     end
   end
 end
