10d9e429e690904584ebf957492d5f1799811043
Bruno Arakaki <brn.arakaki@gmail.com>
2021-05-26 22:53:48 -0300

    Double quotes strings

--- test/pier_logging/helper/redactor_test.rb
+++ test/pier_logging/helper/redactor_test.rb
@@ -1,43 +1,43 @@
-require 'test_helper'
+require "test_helper"
 
 class PierLogging::Helpers::RedactorTest < Minitest::Test
   subject { PierLogging::Helpers::Redactor }
 
-  context '.redact' do
+  context ".redact" do
     setup do
       PierLogging.request_logger_configuration.sensitive_keywords = [:sensitive_key]
     end
-    context 'with a hash' do
+    context "with a hash" do
       setup do
         @hash = {
           sensitive_key: "Amar é fogo que arde sem se ver",
-          not_sensitive: 'Vai Rabetão tão tão no chão',
-          password: 'Que não seja imortal, posto que é chama'
+          not_sensitive: "Vai Rabetão tão tão no chão",
+          password: "Que não seja imortal, posto que é chama"
         }
       end
-      should 'redact only sensitive stuff' do
+      should "redact only sensitive stuff" do
         response = subject.redact(@hash)
 
-        assert_equal '*', response[:sensitive_key]
-        assert_equal 'Vai Rabetão tão tão no chão', response[:not_sensitive]
-        assert_equal '*', response[:password]
+        assert_equal "*", response[:sensitive_key]
+        assert_equal "Vai Rabetão tão tão no chão", response[:not_sensitive]
+        assert_equal "*", response[:password]
       end
     end
 
-    context 'with an array of hashs' do
+    context "with an array of hashs" do
       setup do
         @array = [
           {sensitive_key: "Amar é fogo que arde sem se ver"},
-          {not_sensitive: 'Vai Rabetão tão tão no chão'},
-          {password: 'Que não seja imortal, posto que é chama'}
+          {not_sensitive: "Vai Rabetão tão tão no chão"},
+          {password: "Que não seja imortal, posto que é chama"}
         ]
       end
-      should 'redact only sensitive stuff' do
+      should "redact only sensitive stuff" do
         response = subject.redact(@array)
 
-        assert_equal '*', response[0][:sensitive_key]
-        assert_equal 'Vai Rabetão tão tão no chão', response[1][:not_sensitive]
-        assert_equal '*', response[2][:password]
+        assert_equal "*", response[0][:sensitive_key]
+        assert_equal "Vai Rabetão tão tão no chão", response[1][:not_sensitive]
+        assert_equal "*", response[2][:password]
       end
     end
   end
