509d9fb303dec5d93b86ab56afc79a403cf4f697
Mauricio Banduk <mauricio.banduk@pier.digital>
2023-04-20 09:46:53 -0300

    Change default branch to main

--- bin/rb_lint.sh
+++ bin/rb_lint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Checking changed files"
-files=$(git diff --name-only origin/master -- | xargs ls -d 2>/dev/null | egrep "\.(rb|spec|rake|rhtml|rjs|rxml|erb)$")
+files=$(git diff --name-only origin/main -- | xargs ls -d 2>/dev/null | egrep "\.(rb|spec|rake|rhtml|rjs|rxml|erb)$")
 echo "Changed files:"
 echo "***"
 echo "$files"
